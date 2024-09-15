8d3d16fcb07566929afc4c015dac0dd47f361f46
João-Vinicius <79458405+Moorg2@users.noreply.github.com>
2024-09-15 17:45:07 -0300

    Add files via upload

---
+++ Jogador.java
@@ -0,0 +1,20 @@
+package ProjetoProgramacao;
+
+public class Jogador {
+    class Jogador(){
+        int vida = 100;
+        int mana = 3;
+
+        void receberDano(int dano){
+            vida-=dano;
+            if (vida<=0){
+                /*acaba o jogo*/
+            }
+            }else{
+                /*turno do jogador 2*/
+            }
+
+
+        }
+    }
+}
