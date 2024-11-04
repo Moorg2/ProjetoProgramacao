7a8b4e42ec23205adaad09ad264bcc300fa0260b
Lucas Andrade de Farias <129793608+LucasAFarias@users.noreply.github.com>
2024-11-04 18:22:35 -0300

    Add files via upload
    switch case com as habilidades

--- CriarCartas.java
+++ CriarCartas.java
@@ -1,7 +1,11 @@
 package ProjetoProgramacao;
 
 
-public class CriarCartas {
+public class CriarCartas extends Criatura {
+public CriarCartas(String nomes, int custoMana, int dano, int resist, String habilidadeEspecial) {
+		super(nomes, custoMana, dano, resist, habilidadeEspecial);
+		// TODO Auto-generated constructor stub
+	}
 // Criaturas
 public static Criatura criarCriatura(String nome) {
     switch (nome) {
@@ -205,4 +209,103 @@ public static Encantamento criarEncantamento(String nome) {
             throw new IllegalArgumentException("Encantamento desconhecido: " + nome);
 }
 }
+
+
+
+public void aplicarHabilidade(String habilidade) {
+    switch (habilidade.toLowerCase()) {
+        case "rapidez":
+            rapidez();
+            break;
+        case "telepatia":
+            telepatia();
+            break;
+        case "forca bruta":
+            forcaBruta();
+            break;
+        case "evolucao":
+            evolucao();
+            break;
+        case "fogo intenso":
+            fogoIntenso();
+            break;
+        case "resistencia":
+            resistencia();
+            break;
+        case "sono":
+            sono();
+            break;
+        case "sombra":
+            sombra();
+            break;
+        case "defesa":
+            defesa();
+            break;
+        case "imunidade":
+            imunidade();
+            break;
+        case "navegacao":
+            navegacao();
+            break;
+        case "agilidade":
+            agilidade();
+            break;
+        case "voo rapido":
+            vooRapido();
+            break;
+        case "protecao psiquica":
+            protecaoPsiquica();
+            break;
+        case "aura":
+            aura();
+            break;
+        case "causar medo":
+            causarMedo();
+            break;
+        case "defesa robusta":
+            defesaRobusta();
+            break;
+        case "furia":
+            furia();
+            break;
+        case "agilidade natural":
+            agilidadeNatural();
+            break;
+        case "raiva":
+            raiva();
+            break;
+        case "furia do mar":
+            furiaDoMar();
+            break;
+        case "libero":
+            libero();
+            break;
+        case "escudo noturno":
+            escudoNoturno();
+            break;
+        case "desgaste":
+            desgaste();
+            break;
+        case "ilusao":
+            ilusao();
+            break;
+        case "intimidacao":
+            intimidacao();
+            break;
+        case "espinhos":
+            espinhos();
+            break;
+        default:
+            System.out.println("Habilidade não encontrada: " + habilidade);
+    }
 }
+
+}
+
+
+
+
+
+
+
+
